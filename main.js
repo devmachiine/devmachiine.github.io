b63d03cdfb0b0d8e7175356e4c4a91837a164169
devmachiine <46693400+devmachiine@users.noreply.github.com>
2021-08-16 15:27:58 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 
+const gha = {
+  gh_user: 'none',
+  repo: 'none',
+  repos: {},
+  set user(name) {
+    this.gh_user = name;
+    fetch(`https://api.github.com/users/${name}/repos`).then(r => r.json()).then(r => {
+      r.map(r => r.name).map(n => {
+        this.repos[n] = n
+        console.log(n)
+      })
+    })
+  }
+};
